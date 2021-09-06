8253d78ad5fd2c4c26d9ae095b65778f1c334195
maxuanquang <maxuanquang@gmail.com>
2021-09-06 17:43:33 +0700

    Update cfg_kitti_fm_joint_custom.py

--- config/cfg_kitti_fm_joint_custom.py
+++ config/cfg_kitti_fm_joint_custom.py
@@ -2,8 +2,8 @@ DEPTH_LAYERS = 50
 POSE_LAYERS = 18
 FRAME_IDS = [0, -1, 1, 's']
 IMGS_PER_GPU = 2
-HEIGHT = 192#320
-WIDTH = 640#1024
+HEIGHT = 320
+WIDTH = 1024
 
 data = dict(
     name = 'kitti',
@@ -14,7 +14,7 @@ data = dict(
     in_path = '/content/drive/MyDrive/Dự án/KITTI Dataset/Raw Data',
     gt_depth_path = '/content/logs/gt.npy',
     png = True,
-    stereo_scale = True if 's' in FRAME_IDS else False,
+    stereo_scale = False,
 )
 
 model = dict(
@@ -49,15 +49,15 @@ validate = True
 
 optimizer = dict(type='Adam', lr=learning_rate, weight_decay=0)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-# lr_config = dict(
-#     policy='step',
-#     warmup='linear',
-#     warmup_iters=500,
-#     warmup_ratio=1.0 / 3,
-#     step=[20,30],
-#     gamma=0.5,
-# )
-lr_config = None
+
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[20,30],
+    gamma=0.5,
+)
 
 checkpoint_config = dict(interval=1)
 log_config = dict(interval=50,
