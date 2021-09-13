324e72395c64c11011fb86dcc56f8d386b8b3238
maxuanquang <maxuanquang@gmail.com>
2021-09-13 11:52:57 +0700

    Update custom config to boost speed

--- config/cfg_kittti_fm_custom.py
+++ config/cfg_kittti_fm_custom.py
@@ -1,19 +1,17 @@
 DEPTH_LAYERS = 50#resnet50
 POSE_LAYERS = 18#resnet18
-FRAME_IDS = [0, -1, 1, 's']#0 refers to current frame, -1 and 1 refer to temperally adjacent frames, 's' refers to stereo adjacent frame.
+FRAME_IDS = [0, 's']#0 refers to current frame, -1 and 1 refer to temperally adjacent frames, 's' refers to stereo adjacent frame.
 IMGS_PER_GPU = 2 #the number of images fed to each GPU
 HEIGHT = 320#input image height
 WIDTH = 1024#input image width
 
 data = dict(
     name = 'kitti',#dataset name
-    split = 'custom',#training split name
+    split = 'exp',#training split name
     height = HEIGHT,
     width = WIDTH,
     frame_ids = FRAME_IDS,
     in_path = '/content/drive/MyDrive/Dự án/KITTI Dataset/Raw Data',#path to raw data
-    # in_path = '/content/eigen_test_split',#path to raw data
-    # gt_depth_path = '/media/sconly/harddisk/data/kitti/kitti_raw/rawdata/gt_depths.npz',#path to gt data
     gt_depth_path = '/content/logs/gt.npy',
     png = True,#image format
     stereo_scale = True if 's' in FRAME_IDS else False,
@@ -30,21 +28,21 @@ model = dict(
     scales = [0, 1, 2, 3],# output different scales of depth maps
     min_depth = 0.1, # minimum of predicted depth value
     max_depth = 100.0, # maximum of predicted depth value
-    depth_pretrained_path = None,# pretrained weights for resnet
-    pose_pretrained_path =  None,# pretrained weights for resnet
-    extractor_pretrained_path = None,# pretrained weights for autoencoder
+    depth_pretrained_path = '/content/logs/resnet50.pth',# pretrained weights for resnet
+    pose_pretrained_path =  '/content/logs/resnet18.pth',# pretrained weights for resnet
+    extractor_pretrained_path = '/content/drive/MyDrive/VinAI/Motion segmentation/FeatDepth/logs-step-1/epoch_11.pth',# pretrained weights for autoencoder
     automask = False if 's' in FRAME_IDS else True,
     disp_norm = False if 's' in FRAME_IDS else True,
     perception_weight = 1e-3,
     smoothness_weight = 1e-3,
 )
 
-resume_from = '/content/logs/fm_depth.pth'#directly start training from provide weights
-# resume_from = None
+# resume_from = '/node01_data5/monodepth2-test/model/ms/ms.pth'#directly start training from provide weights
+resume_from = None
 finetune = None
-total_epochs = 100
+total_epochs = 40
 imgs_per_gpu = IMGS_PER_GPU
-learning_rate = 0
+learning_rate = 1e-4
 workers_per_gpu = 4
 validate = True
 
